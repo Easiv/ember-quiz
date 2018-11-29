607551e568d8197f65731cdd340917a23b5f8dec
Easiv <easiv89@gmail.com>
2018-11-29 15:13:53 +0100

    form creating record

--- app/components/submit-form/component.js
+++ app/components/submit-form/component.js
@@ -1,15 +1,28 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   classNames: ['submit-form'],
+  store: service(),
+  router: service('-routing'),
 
   actions: {
     submitForm() {
-
+      let rec = this.store.createRecord('question', {
+        title: this.title,
+        a: this.a,
+        b: this.b,
+        c: this.c,
+        d: this.d,
+        correct: this.correct,
+        customText: this.customText
+      }).save().then(() => {
+        this.actions.redirect();
+      });
     },
 
-    clearForm() {
-
+    redirect() {
+      this.get('router').transitionTo('questions');
     }
   }
 });
